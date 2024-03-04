e42694dc9f65a8aa3b3cd82103e294781fca43c6
tam4x <pierstudy@web.de>
2024-03-04 08:24:04 +0100

    added cnn_model

--- models.py
+++ models.py
@@ -27,3 +27,38 @@ class mnist10_modelv0(nn.Module):
     
     def forward(self, x):
         return self.layer_stack(x)
+    
+
+
+class mnistt10_CNN(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape:int):
+        super().__init__()
+        ## Create multiple conv blocks with a convolution, activation function and a non-linear filter MaxPool for example
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units,
+                      kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units,
+                      kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2) # stride value same as kernel size so that it moves across a value only ones
+        )
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units,
+                      kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units,
+                      kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2) # stride value same as kernel size so that it moves across a value only ones
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*8*8, out_features=output_shape)
+        )
+
+    
+    def forward(self,x):
+        x = self.conv_block1(x) # (image is 32x32) after that 16x16
+        x = self.conv_block2(x) # features are 16x16 after maxpool 8x8
+        return self.classifier(x) # flatten will take in 8x8x10 numbers
